9ce7a774d518584a85a8d19235578376a6fe18fd
Karina Telles <karinac.telles@gmail.com>
2020-10-07 18:41:55 -0300

    floating action button

---
+++ main.js
@@ -0,0 +1,5 @@
+//Floating Action Button
+const elementButton = document.querySelectorAll(".fixed-action-btn");
+const floatingButton = M.FloatingActionButton.init(elementButton, {
+  direction: "left",
+});
